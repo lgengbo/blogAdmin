4954445b4d552832e8f23ab4f4826aa3ea750bf2
lgb <113897051@qq.com>
2020-01-09 17:55:57 +0800

    配置接口

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -2,6 +2,12 @@ const ip = 'http://127.0.0.1:7001/admin/'
 
 const servicePath = {
     checkLogin : ip + 'checkLogin', // 登录
+    getTypeInfo : ip + 'getTypeInfo', // 文章类型
+    addArticle : ip + 'addArticle', // 添加文章
+    updateArticle : ip + 'updateArticle', // 更新文章
+    delArticle : ip + 'delArticle/', // 删除文章
+    getArticleList : ip + 'getArticleList', // 获取文章列表
+    getArticleById : ip + 'getArticleById/', // 修改文章
 }
 
 export default servicePath;
